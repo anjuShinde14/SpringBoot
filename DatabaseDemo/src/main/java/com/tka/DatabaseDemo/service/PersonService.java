3c4241e03f88f3bc11442650d43f95475a8d2e23
Anajli Shinde <157095707+anjuShinde14@users.noreply.github.com>
2024-04-24 18:24:08 +0530

    Add files via upload

---
+++ DatabaseDemo/src/main/java/com/tka/DatabaseDemo/service/PersonService.java
@@ -0,0 +1,45 @@
+package com.tka.DatabaseDemo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+import com.tka.DatabaseDemo.dao.PersonDao;
+import com.tka.DatabaseDemo.entity.Person;
+
+@Service
+public class PersonService {
+   
+	@Autowired
+	PersonDao dao;
+	public String InsertData(Person p) {
+		
+		String msg=dao.insertData(p);
+		return msg;
+	}
+	
+	public String UpdateData(int id, Person p) {
+		
+		String  msg=dao.updateDta(id, p);
+		return msg;
+		}
+	
+	public String DeleteData(int id) {
+		
+		String msg=dao.deleteData(id);
+		return msg;
+		
+	}
+	
+	public Person getParticularRecord(int id) {
+		Person p=dao.getParticularRecord(id);
+		return p;
+	}
+	
+	public List<Person>getAllRecord(){
+		List<Person>list=dao.getAllRecord();
+		return list;
+	}
+	
+}
